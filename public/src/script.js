5b25decd78904153f2a6619f23590c6548ae25dd
cliffmieser <cliffmieser@gmail.com>
2023-07-26 22:36:27 -0400

    script changes

--- public/src/script.js
+++ public/src/script.js
@@ -1,17 +1,2 @@
-const url = require("url")
-
 console.log("External Javascript connected!")
-
-
-const navLinks = document.querySelectorAll(".nav-link");
-console.log(navLinks);
-
-
-let i=0;
-let currentPage = url[url.length - 1];
-
-// navLinks.forEach((link)=>{
-
-//     link.classList.add("active");
-// })
 
